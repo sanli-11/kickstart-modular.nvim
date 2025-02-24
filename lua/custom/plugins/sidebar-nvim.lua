5315045fcf30af323a80329b85a1efa9fea6232d
sanli-11 <sanli.has11@gmail.com>
2025-02-24 10:19:56 +0500

    added plugin Sidebar Nvim

---
+++ lua/custom/plugins/sidebar-nvim.lua
@@ -0,0 +1,22 @@
+return {
+  'sidebar-nvim/sidebar.nvim',
+  opts = {
+    disable_default_keybindings = 0,
+    bindings = nil,
+    open = false,
+    side = 'right',
+    initial_width = 50,
+    hide_statusline = false,
+    update_interval = 1000,
+    sections = { 'datetime', 'git', 'diagnostics' },
+    section_separator = { '', '-----', '' },
+    section_title_separator = { '' },
+    containers = {
+      attach_shell = '/bin/sh',
+      show_all = true,
+      interval = 5000,
+    },
+    datetime = { format = '%a %b %d, %H:%M', clocks = { { name = 'local' } } },
+    todos = { ignored_paths = { '~' } },
+  },
+}
