5c092121ecb73b10d27c6a401e63d7185c408f1e
San Li <sanli.has11@gmail.com>
2025-03-06 13:30:00 +0500

    Added plugin Go To Preview

---
+++ lua/custom/plugins/goto-preview.lua
@@ -0,0 +1,11 @@
+return {
+  'dnlhc/glance.nvim',
+  cmd = 'Glance',
+}
+
+-- gpd -> Preview Definition
+-- gpt -> Preview Type Definition
+-- gpi -> Preview Implementation
+-- gpD -> Preview Declaration
+-- gpr -> Preview References
+-- gpD -> Close All Win
