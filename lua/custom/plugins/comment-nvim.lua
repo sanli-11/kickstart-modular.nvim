b54ed4241a8129375f3e3a25f065e58f93087405
San Li <sanli.has11@gmail.com>
2025-03-07 08:15:32 +0500

    Added plugin Comment Nvim

---
+++ lua/custom/plugins/comment-nvim.lua
@@ -0,0 +1,11 @@
+return {
+  'numToStr/Comment.nvim',
+  opts = {
+    -- add any options here
+  },
+}
+
+-- gb   -> comment block
+-- gco  -> add comment above
+-- gcO  -> add comment below
+-- gcA  -> add comment at end of line
